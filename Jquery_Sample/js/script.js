8309dc826b9932d77fe741a96a00446880dc3cfb
Omoike Sarah Igho <31732886+Sayrai@users.noreply.github.com>
2018-03-09 15:16:18 +0100

    --updates

---
+++ Jquery_Sample/js/script.js
@@ -0,0 +1,12 @@
+function makeRequests() {
+    $.get({
+            url: 'http://httpbin.org/user-agent',
+            dataType: 'json'
+        })
+        .done(function(response) {
+            alert(JSON.stringify(response));
+            $('body > p').text(
+                response['user-agent']
+            );
+        });
+}
---
+++ htmlJavascript/script.js
@@ -0,0 +1,9 @@
+function startLogic() {
+    startLogic();
+    var newHtml = '<em>New Header</em>';
+    document.getElementById('headerItem').innerHTML = newHtml;
+}
+
+function clickHandler() {
+    alert('You clicked me!');
+}
